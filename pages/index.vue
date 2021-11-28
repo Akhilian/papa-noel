f24598ba7003921a8721698baa8681cf975f6b92
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2021-11-28 23:06:56 +0100

    Creating a result page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Tutorial />
+  <div class="relative flex items-top justify-center min-h-screen items-center bg-christmas bg-cover bg-center text-gray-600">
+      <div class="flex flex-col space-x-4 md:flex-row bg-gray-50 p-4 rounded-md shadow-lg max-w-screen-md">
+          <div class="flex justify-center">
+              <img src="/img/santa-claus.svg" class="w-16" />
+          </div>
+          <div class="flex-1 space-y-2">
+            <h1 class="text-2xl font-medium">Oh oh oh !</h1>
+            <p>Mes petits lutins ont tellement travaillé cette année que j'aurais bien besoin de ton aide pour distribuer des cadeaux à tous les enfants qui ont été sage.</p>
+            <p>Cette année, tu seras la joyeux Papa Noël de <strong>Catherine</strong>.</p>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script lang="ts">
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundImage: {
+                'christmas': "url('/img/background.jpg')",
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
